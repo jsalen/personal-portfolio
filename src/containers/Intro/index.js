8da228172b23c0be195712264bfc368e94107215
Joseph Salen <jsalen.91@gmail.com>
2022-01-13 11:52:29 -0400

    change Intro component to make it mobile first and added Content

--- src/containers/Intro/index.js
+++ src/containers/Intro/index.js
@@ -1,11 +1,17 @@
-import { Container } from './styles'
+import { Container, Description, Figure } from './styles'
+import profilePic from '../../statics/images/profile.png'
 
 export const Intro = () => {
   return (
     <Container>
-      <h1>React.js Front-end Developer</h1>
-      <p>Hi there! 👋</p>
-      <p>My name is Joseph Salen</p>
+      <Figure>
+        <img src={profilePic} alt='Foto de perfil' />
+      </Figure>
+
+      <Description>
+        <h1>React.js Front-end Developer</h1>
+        <p>I build beautiful interfaces with the help of React.js</p>
+      </Description>
     </Container>
   )
 }
--- src/containers/Intro/styles.js
+++ src/containers/Intro/styles.js
@@ -2,10 +2,31 @@ import styled from 'styled-components'
 
 export const Container = styled.section`
   width: 100%;
+  height: calc(100vh - 90px);
   padding: 2rem 1.6rem;
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  place-items: center;
+`
+
+export const Figure = styled.figure`
+  & img {
+    width: 189px;
+    object-fit: cover;
+    border: 5px solid #645dd7;
+    border-radius: 50%;
+  }
+`
+
+export const Description = styled.article`
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
 
   & h1 {
-    font-size: 3.2rem;
+    font-size: 3.1rem;
     line-height: 3.6rem;
     font-weight: 700;
     text-align: center;
