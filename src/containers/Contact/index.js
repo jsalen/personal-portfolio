71c722e73e6a598b6ddf3028309fedc71a291c98
Joseph Salen <jsalen.91@gmail.com>
2022-01-14 12:35:14 -0400

    create Contact container

---
+++ src/containers/Contact/index.js
@@ -0,0 +1,17 @@
+import { Button } from '../../components/Button'
+
+import { Container } from './styles'
+
+export const Contact = () => {
+  return (
+    <Container>
+      <h1>Get In Touch!</h1>
+      <p>
+        Currently I'm looking for work and new opportunities, my email is always
+        open. If you are interested in having a chat I'd love to get in touch
+        with you.
+      </p>
+      <Button>Send Email</Button>
+    </Container>
+  )
+}
---
+++ src/containers/Contact/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  width: 100%;
+  height: 100%;
+  padding: 0 2rem 4rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.8rem;
+  color: #645dd7;
+
+  & h1 {
+    font-size: 2.6rem;
+    font-weight: 700;
+    text-align: center;value
+  }
+
+  & p {
+    font-size: 1.6rem;
+    font-weight: 400;
+    line-height: 2.2rem;
+  }
+
+  & button {
+    width: 80%;
+    max-width: 280px;
+  }
+`
