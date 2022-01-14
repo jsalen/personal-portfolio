6fff92a0f0fb4c1d3b5a4ecf36ffbd71eca15f31
Joseph Salen <jsalen.91@gmail.com>
2022-01-14 16:54:11 -0400

    fix some typos

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -9,21 +9,22 @@ export const About = () => {
           Hi there! 👋 my name is <span>Joseph Salen</span>, I'm a React.js
           Front-end Developer currently living in Venezuela. For the last two
           years I've been learning how to create beautiful and powerful websites
-          to bring the life of awesome products to the Internet. My goal is to
+          to bring to life awesome products to the Internet. My goal is to
           translate your idea into a Website that not only looks pretty but also
-          become easy to use for your user.
+          becomes easy to use for your user.
         </p>
         <p>
-          Every day I try to learn something new to add to my skillset, I can be
-          something related to frontend or backend development, but my specialty
-          is Front-End Development.
+          Every day I try to learn something new to add to my skillset, I'm
+          capable of working with' something related to frontend or backend
+          development, but my specialty is Front-End Development.
         </p>
         <p>
-          I've been using awesome platform like{' '}
+          I've been using awesome platforms like{' '}
           <a href='https://www.platzi.com/' target='_blank' rel='noreferrer'>
             Platzi
-          </a>{' '}
-          among others to grind my road into learning Frontend Development.
+          </a>
+          among others to grind my road into learning Frontend Development with
+          ReactJS.
         </p>
         <p>Here are some of the technologies I've been working recently:</p>
         <ul>
