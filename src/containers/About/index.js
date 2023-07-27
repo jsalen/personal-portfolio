d3441bde78adf56cb8274c316955ea71d6147256
Joseph M Salen P <jsalen.91@gmail.com>
2023-07-27 10:56:00 -0400

    ui: update About me section (#4)

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,3 +1,13 @@
+import { FaCss3Alt, FaHtml5, FaReact, FaNodeJs } from "react-icons/fa";
+import { DiJavascript } from "react-icons/di";
+import { TbBrandNextjs } from "react-icons/tb";
+import {
+  SiContentful,
+  SiMongodb,
+  SiStyledcomponents,
+  SiTypescript,
+} from "react-icons/si";
+import { PiFileSqlBold } from "react-icons/pi";
 import { Container, Content } from "./styles";
 
 export const About = () => {
@@ -5,41 +15,72 @@ export const About = () => {
     <Container>
       <h2>About Me</h2>
       <Content>
-        I am a dedicated Frontend Engineer at Apply Digital, specializing in
-        Frontend development with React using NextJS framework. With a
-        background in IT Technician roles since 2016, I made a seamless
-        transition into the world of Frontend development in March 2020,
-        igniting my passion for creating exceptional user experiences. My
-        expertise lies in crafting robust and intuitive Frontend solutions using
-        NextJS. Leveraging my skills, I excel in building reusable components
-        and implementing the principles of atomic design. I have a strong
-        command over integrating Headless CMS, with Contentful being my go-to
-        choice. Continuously seeking growth, I consistently explore new tools
-        and frameworks to enhance my development prowess. What sets me apart in
-        my field is my unwavering dedication to delivering top-notch products to
-        my clients. I bring a high level of enthusiasm and a customer-centric
-        approach to every project, ensuring that the end result not only meets
-        expectations but exceeds them. I take pride in my ability to collaborate
-        effectively with cross-functional teams and assist others whenever
-        needed. Looking ahead, I have ambitious aspirations to evolve into a
-        Full Stack Developer. While my current focus lies in mastering Frontend
-        Development, I am also actively expanding my skill set by diving into
-        the world of React Native. By acquiring versatile expertise, I aim to
-        contribute to innovative projects and make a significant impact in the
-        realm of software development.
+        <p>
+          I am a dedicated Frontend Engineer with 3 years of experience. My most
+          recent company was Apply Digital, where I worked for over a year. I
+          specialize in Frontend development with React using the NextJS
+          framework. I have a background as IT Technician since 2016. In March
+          2020, I made a seamless transition into the world of Frontend
+          development. This ignited my passion for creating exceptional user
+          experiences.
+        </p>
+        <p>
+          My expertise lies in crafting robust and intuitive frontend solutions
+          using Next.js. I excel in building reusable components and
+          implementing the principles of atomic design. I have a strong command
+          over integrating headless CMS, with Contentful being my go-to choice.
+          I am continuously seeking growth and consistently explore new tools
+          and frameworks to enhance my development prowess. What sets me apart
+          in my field is my unwavering dedication to delivering top-notch
+          products to my clients
+        </p>
+        <p>
+          I bring a high level of enthusiasm and a customer-centric approach to
+          every project, ensuring that the end result not only meets but exceeds
+          expectations. I take pride in my ability to collaborate effectively
+          with cross-functional teams and assist others whenever needed. Looking
+          ahead, I have ambitious aspirations to evolve into a Full Stack
+          Developer. While my current focus lies in mastering Frontend
+          Development.
+        </p>
         <p>Here are some of the technologies I've been working recently:</p>
         <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>SASS</li>
-          <li>JavaScript</li>
-          <li>React.js</li>
-          <li>TypeScript</li>
-          <li>Style-Components</li>
-          <li>Node.js</li>
-          <li>Express</li>
-          <li>MongoDB</li>
-          <li>PosgreSQL</li>
+          <li>
+            <FaHtml5 /> HTML
+          </li>
+          <li>
+            <FaCss3Alt /> CSS
+          </li>
+          <li>
+            <DiJavascript /> JavaScript
+          </li>
+          <li>
+            <FaReact /> React.js
+          </li>
+          <li>
+            <TbBrandNextjs /> Next.js
+          </li>
+          <li>
+            <SiTypescript /> TypeScript
+          </li>
+          <li>
+            <SiStyledcomponents /> Style-Components
+          </li>
+          <li>
+            <FaNodeJs /> Node.js
+          </li>
+          <li>
+            <FaNodeJs /> Express
+          </li>
+          <li>
+            <SiMongodb /> MongoDB
+          </li>
+          <li>
+            <PiFileSqlBold /> PosgreSQL
+          </li>
+          <li>
+            <SiContentful /> Contentful CMS
+          </li>
         </ul>
       </Content>
     </Container>
